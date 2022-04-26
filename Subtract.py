206af1bc146cf8f5b0e2c61eefb903c01b059176
smitd21 <smitdesai2130@gmail.com>
2022-04-26 22:35:22 +0530

    first commit

---
+++ Subtract.py
@@ -0,0 +1,8 @@
+
+#Python program to subtract two numbers
+num1=90; #variable declaration and initialization
+num2=50;
+#Subtract two numbers
+sub=num2-num1;
+#display the result of substraction
+print("The subtract of {0} and {1} is {2}".format(num2,num1,sub) )
